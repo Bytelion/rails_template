1a9b2101e78455f5b4505672196a1154d2b8796b
Cooper Luetje <cooper@bytelion.com>
2020-12-07 22:50:30 -0500

    Rubocop auto corrections

--- app/controllers/v1/passwords_controller.rb
+++ app/controllers/v1/passwords_controller.rb
@@ -1,20 +1,20 @@
-class V1::PasswordsController < DeviseTokenAuth::PasswordsController
-  before_action :validate_redirect_url_param, only: [:create, :edit]
-  skip_before_action :verify_authenticity_token, :only => [:create, :edit, :update]
+# frozen_string_literal: true
 
-  def edit
-    super
-  end
+module V1
+  class PasswordsController < DeviseTokenAuth::PasswordsController
+    before_action :validate_redirect_url_param, only: %i[create edit]
+    skip_before_action :verify_authenticity_token, only: %i[create edit update]
 
-  private
+    private
 
-  def validate_redirect_url_param
-    # give redirect value from params priority
-    @redirect_url = params.fetch(
-      :redirect_url,
-      DeviseTokenAuth.default_password_reset_url
-    )
+    def validate_redirect_url_param
+      # give redirect value from params priority
+      @redirect_url = params.fetch(
+        :redirect_url,
+        DeviseTokenAuth.default_password_reset_url
+      )
 
-    return render_create_error_missing_redirect_url unless @redirect_url
+      return render_create_error_missing_redirect_url unless @redirect_url
+    end
   end
 end
